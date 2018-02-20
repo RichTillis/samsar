530a010bf436b126ba1d6b4299550068d9560e90
Rich <rich.tillis@gmail.com>
2018-02-19 19:25:19 -0700

    updated news cards

---
+++ js/news-slideshow.js
@@ -0,0 +1,35 @@
+$('#rescue-training').click(function (e) {
+	e.preventDefault();
+	$.swipebox([
+		{ href: './images/news/full/training1.jpg', title: 'Specialized Rescue Training' },
+		{ href: './images/news/full/training2.jpg', title: 'Specialized Rescue Training' },
+		{ href: './images/news/full/training3.jpg', title: 'Specialized Rescue Training' },
+		{ href: './images/news/full/training4.jpg', title: 'Specialized Rescue Training' }
+	]);
+});
+
+$('#7FallsRescue').click(function (e) {
+	e.preventDefault();
+	$.swipebox([
+		{ href: './images/news/full/7FallsRescue1.jpg', title: '7 Falls Rescue December 2017' },
+		{ href: './images/news/full/7FallsRescue2.jpg', title: '7 Falls Rescue December 2017' },
+		{ href: './images/news/full/7FallsRescue3.jpg', title: '7 Falls Rescue December 2017' }
+	]);
+});
+
+$('#gpstraining').click(function (e) {
+	e.preventDefault();
+	$.swipebox([
+		{ href: './images/news/full/gps1.jpg', title: 'GPS Training' },
+		{ href: './images/news/full/gps2.jpg', title: 'GPS Training' },
+		{ href: './images/news/full/gps3.jpg', title: 'GPS Training' },
+		{ href: './images/news/full/gps4.jpg', title: 'GPS Training' }
+	]);
+});
+
+$('#overheating').click(function (e) {
+	e.preventDefault();
+	$.swipebox([
+		{ href: './images/news/full/overheating.jpg', title: 'Signs and Symptoms' }
+	]);
+});
--- js/rescue-training.js
+++
@@ -1,9 +0,0 @@
-$( '#rescue-training' ).click( function( e ) {
-	e.preventDefault();
-	$.swipebox( [
-		{ href:'./images/training1.jpg', title:'Specialized Rescue Training' }, 
-        { href:'./images/training2.jpg', title:'Specialized Rescue Training' },
-        { href:'./images/training3.jpg', title:'Specialized Rescue Training' },
-        { href:'./images/training4.jpg', title:'Specialized Rescue Training' }
-	] );
-} );

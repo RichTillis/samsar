9ca5c941e697c769a214085db048424eaf9452b3
Rich <rich.tillis@gmail.com>
2018-04-30 19:32:46 -0700

    added horse rescue news card and pics

--- js/news-slideshow.js
+++ js/news-slideshow.js
@@ -1,9 +1,9 @@
 $('#rescue-training').click(function (e) {
 	e.preventDefault();
 	$.swipebox([
-		{ href: './images/news/full/training1.jpg', title: 'Specialized Rescue Training' },
+		// { href: './images/news/full/training1.jpg', title: 'Specialized Rescue Training' },
 		{ href: './images/news/full/training2.jpg', title: 'Specialized Rescue Training' },
-		{ href: './images/news/full/training3.jpg', title: 'Specialized Rescue Training' },
+		// { href: './images/news/full/training3.jpg', title: 'Specialized Rescue Training' },
 		{ href: './images/news/full/training4.jpg', title: 'Specialized Rescue Training' }
 	]);
 });
@@ -31,5 +31,24 @@ $('#overheating').click(function (e) {
 	e.preventDefault();
 	$.swipebox([
 		{ href: './images/news/full/overheating.jpg', title: 'Signs and Symptoms' }
+	]);
+});
+
+$('#latr').click(function (e) {
+	e.preventDefault();
+	$.swipebox([
+		{ href: './images/news/full/latr1.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr4.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr5.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr6.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr7.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr8.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr9.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr10.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr11.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr12.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr13.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr14.jpg', title: 'Large Animal Rescue Training' },
+		{ href: './images/news/full/latr15.jpg', title: 'Large Animal Rescue Training' },
 	]);
 });
